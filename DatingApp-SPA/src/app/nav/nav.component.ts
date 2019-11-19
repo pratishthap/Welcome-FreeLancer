fb190c4cf7c9fc074f9d82487b98b9ab5121d98f
Prat <pratishtha.painuly@gmail.com>
2019-11-19 13:29:50 -0500

    navigation comit

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -9,7 +10,8 @@ import { AlertifyService } from '../_services/alertify.service';
 })
 export class NavComponent implements OnInit {
  model: any = {};
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService ,
+              private router: Router) { }
 
   ngOnInit() {
   }
@@ -19,6 +21,8 @@ export class NavComponent implements OnInit {
       this.alertify.success('Logged in successfully');
     }, error => {
       this.alertify.error(error);
+    }, () => {
+        this.router.navigate(['/members'])
     });
   }
 
@@ -29,6 +33,7 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.success('logged out');
+    this.router.navigate(['/home']);
   }
 
 }
